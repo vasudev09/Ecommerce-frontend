db9f2bad9cb4f8dffba3569fc4fdcaa61812eb17
vasudev09 <vasudevaraju2525@gmail.com>
2024-08-03 15:06:13 +0530

    added checkout page and contact page

--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -16,39 +16,39 @@ const OrderPage = async ({ params }: { params: { id: string } }) => {
   } else {
     return (
       <div className="flex flex-col h-[100vh] items-center justify-center">
-        {/* <div className="shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] px-40 py-20"> */}
-        <h1 className="text-xl">Order Details</h1>
-        <div className="mt-12 flex flex-col gap-6">
-          <div className="">
-            <span className="font-medium">Order Id: </span>
-            <span>{"order id"}</span>
-          </div>
-          <div className="">
-            <span className="font-medium">Receiver Name: </span>
-            <span>{"first name + last name"}</span>
-          </div>
-          <div className="">
-            <span className="font-medium">Receiver Email: </span>
-            <span>{"email"}</span>
-          </div>
-          <div className="">
-            <span className="font-medium">Price: </span>
-            <span>{"amount"}</span>
-          </div>
-          <div className="">
-            <span className="font-medium">Payment Status: </span>
-            <span>{"status"}</span>
-          </div>
-          <div className="">
-            <span className="font-medium">Order Status: </span>
-            <span>{"status"}</span>
-          </div>
-          <div className="">
-            <span className="font-medium">Delivery Address: </span>
-            <span>{"address"}</span>
+        <div className="md:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] md:px-40 md:py-20">
+          <h1 className="text-xl">Order Details</h1>
+          <div className="mt-12 flex flex-col gap-6">
+            <div className="">
+              <span className="font-medium">Order Id: </span>
+              <span>{"order id"}</span>
+            </div>
+            <div className="">
+              <span className="font-medium">Receiver Name: </span>
+              <span>{"first name + last name"}</span>
+            </div>
+            <div className="">
+              <span className="font-medium">Receiver Email: </span>
+              <span>{"email"}</span>
+            </div>
+            <div className="">
+              <span className="font-medium">Price: </span>
+              <span>{"amount"}</span>
+            </div>
+            <div className="">
+              <span className="font-medium">Payment Status: </span>
+              <span>{"status"}</span>
+            </div>
+            <div className="">
+              <span className="font-medium">Order Status: </span>
+              <span>{"status"}</span>
+            </div>
+            <div className="">
+              <span className="font-medium">Delivery Address: </span>
+              <span>{"address"}</span>
+            </div>
           </div>
         </div>
-        {/* </div> */}
         <Link href="/" className="underline mt-6">
           Have a problem? Contact us
         </Link>
